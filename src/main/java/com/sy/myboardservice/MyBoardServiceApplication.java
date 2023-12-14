be31cbef0f862ada7222f92c2fb548fa4576a3a8
youswim <youswim96@gmail.com>
2023-12-14 21:25:28 +0900

    init

---
+++ src/main/java/com/sy/myboardservice/MyBoardServiceApplication.java
@@ -0,0 +1,13 @@
+package com.sy.myboardservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyBoardServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyBoardServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sy/myboardservice/MyBoardServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sy.myboardservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyBoardServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
