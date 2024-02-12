c34e4f08691a3328e6cd7cf8609536cb472a1801
youswim <youswim96@gmail.com>
2024-02-12 22:46:27 +0900

    :wrench: config: insert 초기화 sql 추가

---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO post (id, title, content) values (1, 'title1', 'content1');
+INSERT INTO post (id, title, content) values (2, 'title2', 'content2');
+INSERT INTO post (id, title, content) values (3, 'title3', 'content3');
